82607451de630953ad8de1050b2f0dc3f8356f85
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-08 11:28:44 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import { Client, Intents } from 'discord.js';
+import dotenv from 'dotenv';
+import { registerCommands } from './utils/registerCommands';
 
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES
+    ],
+});
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+    await registerCommands(client);
+});
+
+client.login(process.env.DISCORD_TOKEN);
